cb54b9ca822b591b680485e43e3a2062670e6314
Dado00 <62186281+Dado00@users.noreply.github.com>
2021-12-01 20:14:29 -0600

    Add files via upload

---
+++ ADA05_Matrices/ADA05_Matrices/src/test/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+import Modelo.Matriz;
+import Vista.Interface;
+/**
+ *
+ * @author dado0
+ */
+public class Test {
+     public static void main(String[] args) {
+     
+        Interface vista= new Interface();
+        vista.setVisible(true);   
+}
+    
+}
